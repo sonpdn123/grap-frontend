000abebf0406629c8a9d407c19a2ef6550fa2a11
sonpdn123 <sonpdn123@gmail.com>
2026-06-13 22:41:13 +0700

    fix

--- sw.js
+++ sw.js
@@ -1,6 +1,37 @@
+const CACHE_NAME = 'grab-income-v2';
+const assets = [
+    '/',
+    '/index.html',
+    '/script.js',
+    '/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
-    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assets);
+        }).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            return cachedResponse || fetch(e.request);
+        })
+    );
 });
